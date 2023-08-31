32a14a1dfbecfbb2ae861a63d2e9a6677d05518f
Dananjay 996 <sreedananjay456@gmail.com>
2023-08-31 22:13:27 +0530

    gridTraveller DP

--- DP/gridTraveller.cpp
+++ DP/gridTraveller.cpp
@@ -1,10 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long gridTravellerDP(int m, int n, vector<vector<long>> &dp){
+    if(n == 0 || m == 0) return 0;
+    if(n == 1 && m == 1) return 1;
+
+    if(dp[m][n] != 0) return dp[m][n];
+
+    dp[m][n] = gridTravellerDP(m-1, n,dp) + gridTravellerDP(m, n-1,dp);
+    return dp[m][n];
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
+    int m,n;
+    cin >> m >> n;
+
+    vector<vector<long>> dp(m+1, vector<long>(n+1, 0));
+    long numberOfWays = gridTravellerDP(m,n,dp);
+    cout << numberOfWays << endl;
     return 0;
 }
+
