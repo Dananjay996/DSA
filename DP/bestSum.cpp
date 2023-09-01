f59488d94f2d98c7eda9a83de6cc63881780e68a
Dananjay 996 <sreedananjay456@gmail.com>
2023-09-01 18:37:29 +0530

    01-09-2023

---
+++ DP/bestSum.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bestSum(vector<int>& array, int target){
+    if(target == 0) return {};
+    if(target < 0) return {-1};
+    vector<int> bestCombination = {};
+
+    for(int num : array){
+        int remainder = target - num;
+        vector<int> remainderCombination = bestSum(array, remainder);
+        if(remainderCombination[0] != -1){
+            vector<int> combination = remainderCombination;
+            combination.push_back(num);
+            if(bestCombination.size() == 0 || combination.size() < bestCombination.size()){
+                bestCombination = combination;
+            }
+        }
+    }
+
+    return bestCombination;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n,target;
+    cin >> n;
+    vector<int> array(n);
+    for(int i=0; i<n; i++) cin >> array[i];
+    cin >> target;
+
+    vector<int> answer = bestSum(array, target);
+
+    for(int num : answer) cout << num << " ";
+
+    return 0;
+}
