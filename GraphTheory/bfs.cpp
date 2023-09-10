3d8d0e4327298329464d32288d204079d8291140
Dananjay 996 <sreedananjay456@gmail.com>
2023-09-10 23:40:45 +0530

    10-09/2023

--- GraphTheory/bfs.cpp
+++ GraphTheory/bfs.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -14,8 +15,7 @@ int main() {
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
-    vector<bool> visited(numberOfNodes+1, false);
-    
+
 
     return 0;
 }
