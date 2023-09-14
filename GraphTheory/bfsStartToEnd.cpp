4714f2dbcc2992f6c11e0f546704d250b49fcc84
Dananjay 996 <sreedananjay456@gmail.com>
2023-09-14 23:47:04 +0530

    14-09/2023

--- GraphTheory/bfsStartToEnd.cpp
+++ GraphTheory/bfsStartToEnd.cpp
@@ -1,3 +1,22 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+int numberOfNodes, numberOfEdges, startNode;
+vector<vector<int>> adjList(numberOfNodes+1);
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> numberOfNodes >> numberOfEdges >> startNode;
+    for(int i=0; i<numberOfEdges; i++){
+        int u,v;
+        cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+}
 
 
