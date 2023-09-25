36e7f422e8db700f7d0342d7b7d3fb51ae0dc6c7
Dananjay 996 <sreedananjay456@gmail.com>
2023-09-25 23:35:04 +0530

    25-09/2023

---
+++ Leetcode/Medium/findDuplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int> mp;
+        
+        for(int num : nums){
+            mp[num]++;
+        }
+
+        for(auto& it : mp){
+            if(it.second > 1){
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
+};
