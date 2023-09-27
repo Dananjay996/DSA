836c67747aec2ec00c1d7c074c5c388c22964eda
Dananjay 996 <sreedananjay456@gmail.com>
2023-09-27 19:45:27 +0530

    27-09/2023

---
+++ GraphTheory/topologicalSort.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<vector<int>> adjList, vector<bool>& visited, int node, vector<int>& visitedNodes){
+    visited[node] = true;
+
+    for(int neighbour : adjList[node]){
+        if(!visited[neighbour]){
+            dfs(adjList, visited, neighbour, visitedNodes);
+        }
+    }
+
+    visitedNodes.push_back(node);
+}
+
+vector<int> topologicalSort(vector<vector<int>> adjList, int startNode){
+    vector<bool> visited(adjList.size(), false);
+    int n = adjList.size();
+    vector<int> ordering;
+    int i = n-1;
+
+    for(int j=0; j<n; j++){
+        if(!visited[j]){
+            vector<int> visitedNodes;
+            dfs(adjList, visited, j, visitedNodes);
+            for(int node: visitedNodes){
+                ordering[i] = node;
+                i--;
+            }
+        }
+    }
+
+    return ordering;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int numberOfNodes, numberOfEdges, startNode;
+    cin >> numberOfNodes >> numberOfEdges >> startNode;
+    vector<vector<int>> adjList(numberOfNodes+1);
+    for(int i=0; i<numberOfEdges; i++){
+        int u,v;
+        cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    } 
+    vector<bool> visited(numberOfNodes+1, false);
+
+    vector<int> ordering = topologicalSort(adjList, startNode);
+
+    for(int node: ordering){
+        cout << node << " ";
+    }
+
+    return 0;
+}
